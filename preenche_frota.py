f4d2abdcd7d85207ff521137baf746fc62e461b8
felipeap3 <felipeap3@al.insper.edu.br>
2023-04-27 10:55:25 -0300

    Preenche Frota

--- preenche_frota.py
+++ preenche_frota.py
@@ -8,10 +8,14 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             lista_posicoes.append([linha,coluna+i])
     return lista_posicoes
 
-def preenche_frota(frota,navio,linha, coluna, orientacao, tamanho):
-    posicao = [define_posicoes(linha,coluna,orientacao,tamanho)]
-    frota[navio] += posicao
+def preenche_frota(frota,navio,linha,coluna,orientacao,tamanho):
+    if navio not in frota:
+        frota[navio] = [define_posicoes(linha,coluna,orientacao,tamanho)]
+    elif navio in frota:
+        frota[navio].append(define_posicoes(linha,coluna,orientacao,tamanho)) 
     return frota
+   
+
 
 
 print(preenche_frota({"navio-tanque":[[[6,1],[6,2],[6,3]]]},'navio-tanque',4,7,'vertical',3))
