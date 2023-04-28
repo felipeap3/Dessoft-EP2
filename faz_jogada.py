7e2be1156197fada22a608df739e76ba9625775c
felipeap3 <felipeap3@al.insper.edu.br>
2023-04-28 08:20:45 -0300

    Faz Jogada

---
+++ faz_jogada.py
@@ -0,0 +1,21 @@
+def faz_jogada(tabuleiro,linha,coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X' 
+    return tabuleiro
+    
+
+
+print(faz_jogada([
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+],1,1))
