b0a357d597f54cd4808919dc5be831c2fe5f13dc
felipeap3 <felipeap3@al.insper.edu.br>
2023-05-03 21:04:06 -0300

    Posiciona Frota

---
+++ posiciona_frota.py
@@ -0,0 +1,43 @@
+def posiciona_frota(frota):
+    tabuleiro = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],]
+    for navio,lista1 in  frota.items():
+        for lista2 in lista1:
+            for coordenadas in lista2:
+                x = coordenadas[0]
+                y = coordenadas[1]
+                tabuleiro[x][y] = 1
+    return tabuleiro
+        
+
+
+
+print(posiciona_frota({
+    "porta-aviões":[
+      [[1,5],[1,6],[1,7],[1,8]]
+    ],
+    "navio-tanque":[
+      [[6,1],[6,2],[6,3]],
+      [[4,7],[5,7],[6,7]]
+    ],
+    "contratorpedeiro":[
+      [[1,1],[2,1]],
+      [[2,3],[3,3]],
+      [[9,1],[9,2]]
+    ],
+    "submarino": [
+      [[0,3]],
+      [[4,5]],
+      [[8,9]],
+      [[8,4]]
+    ],
+}))
